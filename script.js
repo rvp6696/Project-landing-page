50ae8738f199c508f5f2947d86c416379fd7ad4c
rohithpatil <rohith@gmail.com>
2023-09-21 22:33:03 +0530

    Landing page created with gsap animation

---
+++ script.js
@@ -0,0 +1,22 @@
+
+gsap.to(".image-container", {
+    ease: Expo.easeInOut,
+    width: "100%",
+    duration: 2,
+    stagger: 2
+})
+
+gsap.to(".text h1", {
+    ease: Expo.easeInOut,
+    duration: 2,
+    stagger: 2,
+    top: 0
+})
+
+gsap.to(".text h1", {
+    delay: 2,
+    ease: Expo.easeInOut,
+    duration: 2,
+    stagger: 2,
+    top: "-100%"
+})
